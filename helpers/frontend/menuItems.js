447121d0b184008c64d743946f202a614c55b6f8
joanaraposo96 <joanaraposo96@outlook.pt>
2026-06-09 19:33:41 +0200

    refactor: move instantiation of menu item classes to the beginning of functions for better readability

--- helpers/frontend/menuItems.js
+++ helpers/frontend/menuItems.js
@@ -10,40 +10,48 @@ import { Users } from "../../frontend/pom/Users";
 
 export async function clickAndAssertStudentMenuItems(page) {
     const header = new Header(page);
+    const books = new Books(page);
+    const favourites = new Favourites(page);
+    const rents = new Rents(page);
+    const booksAvailableForPurchase = new BooksAvailableForPurchase(page);
+    const purchaseHistory = new PurchaseHistory(page);
+    const dashboard = new Dashboard(page);
 
     await header.openBooks();
-    const books = new Books(page);
+
     await books.expectPageLoaded();
 
     await header.openFavourites();
-    const favourites = new Favourites(page);
+
     await favourites.expectPageLoaded();
 
     await header.openRents();
-    const rents = new Rents(page);
+
     await rents.expectPageLoaded();
 
     await header.openPurchases();
-    const booksAvailableForPurchase = new BooksAvailableForPurchase(page);
+
     await booksAvailableForPurchase.expectPageLoaded();
 
     await header.openPersonalPurchases();
-    const purchaseHistory = new PurchaseHistory(page);
+
     await purchaseHistory.expectPageLoaded();
 
     await header.openDashboard();
-    const dashboard = new Dashboard(page);
-    dashboard.expectPageLoaded();
+
+    await dashboard.expectPageLoaded();
 }
 
 export async function clickAndAssertAdminMenuItems(page) {
     const header = new Header(page);
+    const adminPurchases = new AdminPurchases(page);
+    const users = new Users(page);
 
     await header.openAdminPurchases();
-    const adminPurchases = new AdminPurchases(page);
+
     await adminPurchases.expectPageLoaded();
 
     await header.openUsers();
-    const users = new Users(page);
+    
     await users.expectPageLoaded();
 }
